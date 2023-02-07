4bd019edfbc3801220985dc2f775e214d91098ef
Justin Drew <jdrew82@users.noreply.github.com>
2023-02-07 11:59:15 -0600

    test: ✅ Fix various inherited tests

--- nautobot_ssot_dna_center/tests/fixtures.py
+++ nautobot_ssot_dna_center/tests/fixtures.py
@@ -4,6 +4,6 @@ from nautobot_ssot_dna_center.models import DNACInstance
 
 def create_dnacinstance():
     """Fixture to create necessary number of DNACInstance for tests."""
-    DNACInstance.objects.create(name="Test One", slug="test-one")
-    DNACInstance.objects.create(name="Test Two", slug="test-two")
-    DNACInstance.objects.create(name="Test Three", slug="test-three")
+    DNACInstance.objects.create(name="Test One", slug="test-one", port=443)
+    DNACInstance.objects.create(name="Test Two", slug="test-two", port=443)
+    DNACInstance.objects.create(name="Test Three", slug="test-three", port=443)
