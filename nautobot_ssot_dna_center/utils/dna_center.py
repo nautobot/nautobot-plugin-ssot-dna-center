867ef0f1f367c146ef7e4c84e9a60a0c87f48429
Justin Drew <jdrew82@users.noreply.github.com>
2023-02-08 20:40:22 -0600

    feat: ✨ Add DnaCenterClient for handling connection to instance.
    Also included methods for getting sites, devices, and finding the site address and type.

--- nautobot_ssot_dna_center/utils/dna_center.py
+++ nautobot_ssot_dna_center/utils/dna_center.py
@@ -1,1 +1,62 @@
 """Utility functions for working with DNA Center."""
+
+import logging
+from dnacentersdk import api
+from dnacentersdk.exceptions import ApiError
+
+LOGGER = logging.getLogger(__name__)
+
+
+class DnaCenterClient:
+    """Client for handling all interactions with DNA Center."""
+
+    def __init__(self, url: str, username: str, password: str, port: int = 443, verify: bool = True):
+        """Initialize instance of client."""
+        self.url = url
+        self.port = port
+        self.base_url = f"{self.url}:{self.port}"
+        self.username = username
+        self.password = password
+        self.verify = verify
+        self.conn = self.connect
+
+    def connect(self):
+        """Connect to Cisco DNA Center."""
+        try:
+            return api.DNACenterAPI(
+                base_url=self.base_url, username=self.username, password=self.password, verify=self.verify
+            )
+        except ApiError as err:
+            LOGGER.error("Unable to connect to DNA Center: %s", err)
+
+    def get_sites(self):
+        """Retrieve all Site data from DNA Center."""
+        try:
+            return self.conn.sites.get_site()["response"]
+        except ApiError as err:
+            LOGGER.error("Unable to get site information from DNA Center. %s", err)
+
+    @staticmethod
+    def find_address_and_type(info: dict):
+        """Find Site address and type from additionalInfo dict.
+
+        Args:
+            info (dict): Site additionalInfo property from DNA Center.
+
+        Returns:
+            tuple: Tuple of Site address and type.
+        """
+        address = ""
+        site_type = ""
+        for element in info:
+            if element["nameSpace"] == "Location":
+                address = element["attributes"]["address"]
+                site_type = element["attributes"]["type"]
+        return (address, site_type)
+
+    def get_devices(self):
+        """Retrieve all Device data from DNA Center."""
+        try:
+            return self.conn.devices.get_device_list()["response"]
+        except ApiError as err:
+            LOGGER.error("Unable to get device information from DNA Center. %s", err)
