5cbbb55589a8d96f46d52db716402cd10ac8c902
Justin Drew <jdrew82@users.noreply.github.com>
2023-02-09 13:43:25 -0600

    feat: ✨ Add load_sites to Nautobot adapter

--- nautobot_ssot_dna_center/diffsync/adapters/nautobot.py
+++ nautobot_ssot_dna_center/diffsync/adapters/nautobot.py
@@ -1,12 +1,17 @@
 """Nautobot Adapter for DNA Center SSoT plugin."""
 
 from diffsync import DiffSync
+from nautobot.dcim.models import Site as OrmSite
+from nautobot_ssot_dna_center.diffsync.models.nautobot import NautobotDevice, NautobotSite
 
 
 class NautobotAdapter(DiffSync):
     """DiffSync adapter for Nautobot."""
 
-    top_level = []
+    site = NautobotSite
+    device = NautobotDevice
+
+    top_level = ["site", "device"]
 
     def __init__(self, *args, job=None, sync=None, **kwargs):
         """Initialize Nautobot.
@@ -19,6 +24,17 @@ class NautobotAdapter(DiffSync):
         self.job = job
         self.sync = sync
 
+    def load_sites(self):
+        """Load Site data from Nautobot into DiffSync models."""
+        for site in OrmSite.objects.all():
+            new_site = self.site(
+                name=site.name,
+                address=site.physical_address,
+                site_type=None,
+                parent=site.region.name,
+            )
+            self.add(new_site)
+
     def load(self):
         """Load data from Nautobot into DiffSync models."""
-        raise NotImplementedError
+        self.load_sites()
