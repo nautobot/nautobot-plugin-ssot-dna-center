9786501c742157e4097928f9688e36b7c4e6d111
Justin Drew <jdrew82@users.noreply.github.com>
2023-03-01 15:34:41 -0600

    test: ✅ Add tests for the NautobotArea class

---
+++ nautobot_ssot_dna_center/tests/test_models_nautobot.py
@@ -0,0 +1,54 @@
+"""Test the DiffSync models for Nautobot."""
+
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from diffsync import DiffSync
+from nautobot.dcim.models import Region
+
+from nautobot_ssot_dna_center.diffsync.models.nautobot import NautobotArea
+
+
+class TestNautobotArea(TestCase):
+    """Test the NautobotArea class."""
+
+    def setUp(self):
+        self.diffsync = DiffSync()
+        self.diffsync.job = MagicMock()
+        self.diffsync.job.log_info = MagicMock()
+        self.diffsync.job.log_warning = MagicMock()
+
+    def test_create(self):
+        """Validate the NautobotArea create() method creates a Region."""
+        Region.objects.create(
+            name="Global",
+        )
+        ids = {"name": "NY", "parent": "Global"}
+        attrs = {}
+        result = NautobotArea.create(self.diffsync, ids, attrs)
+        self.assertIsInstance(result, NautobotArea)
+        self.diffsync.job.log_info.assert_called_once_with(message="Creating Region NY.")
+        region_obj = Region.objects.get(name=ids["name"])
+        self.assertEqual(region_obj.parent.name, ids["parent"])
+
+    def test_create_missing_parent(self):
+        """Validate the NautobotArea create() method with missing parent Region."""
+        ids = {"name": "TX", "parent": "USA"}
+        attrs = {}
+        NautobotArea.create(self.diffsync, ids, attrs)
+        self.diffsync.job.log_warning.assert_called_once_with(
+            message="Unable to find Region USA for TX. Region matching query does not exist."
+        )
+
+    def test_delete(self):
+        ds_mock_region = MagicMock(spec=Region)
+        ds_mock_region.uuid = "1234567890"
+        ds_mock_region.diffsync = MagicMock()
+        ds_mock_region.diffsync.job.log_info = MagicMock()
+        mock_region = MagicMock(spec=Region)
+        mock_region.name = "Test"
+        region_get_mock = MagicMock(return_value=mock_region)
+        with patch.object(Region.objects, "get", region_get_mock):
+            result = NautobotArea.delete(ds_mock_region)
+        ds_mock_region.diffsync.job.log_info.assert_called_once_with(message="Deleting Region Test.")
+        self.assertEqual(ds_mock_region, result)
