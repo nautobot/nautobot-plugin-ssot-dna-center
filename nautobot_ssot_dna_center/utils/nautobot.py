1456789261ad63d33d82b28e9210006691169d90
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-04-07 11:36:47 -0500

    feat: ✨ Create verify_platform function to get/create Platform to ensure standardized.
    This should ensure that all Platforms have the name, slug, and NAPALM driver filled in using the netutils mappers. The standard is to have the Platfrom name be the Ansible collection, slug is netmiko driver, and NAPALM driver is obvious. If none of these apply it uses the name passed to it.

--- nautobot_ssot_dna_center/utils/nautobot.py
+++ nautobot_ssot_dna_center/utils/nautobot.py
@@ -1,1 +1,33 @@
 """Utility functions for working with Nautobot."""
+from netutils.lib_mapper import ANSIBLE_LIB_MAPPER_REVERSE, NAPALM_LIB_MAPPER_REVERSE
+from nautobot.dcim.models import Platform
+def verify_platform(platform_name: str, manu: UUID) -> Platform:
+    """Verifies Platform object exists in Nautobot. If not, creates it.
+
+    Args:
+        platform_name (str): Name of platform to verify.
+        manu (UUID): The ID (primary key) of platform manufacturer.
+
+    Returns:
+        Platform: Found or created Platform object.
+    """
+    if ANSIBLE_LIB_MAPPER_REVERSE.get(platform_name):
+        _name = ANSIBLE_LIB_MAPPER_REVERSE[platform_name]
+    else:
+        _name = platform_name
+    if NAPALM_LIB_MAPPER_REVERSE.get(platform_name):
+        napalm_driver = NAPALM_LIB_MAPPER_REVERSE[platform_name]
+    else:
+        napalm_driver = platform_name
+    try:
+        platform_obj = Platform.objects.get(slug=platform_name)
+    except Platform.DoesNotExist:
+        platform_obj = Platform(
+            name=_name,
+            slug=platform_name,
+            manufacturer_id=manu,
+            napalm_driver=napalm_driver[:50],
+        )
+        platform_obj.validated_save()
+    return platform_obj
+
