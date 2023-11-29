c3d62faf17458dabe3cc2ef57a95e27e7fa9cef7
Swanand Tambe <swanandvt@gmail.com>
2023-11-29 13:22:15 -0500

    fix: Navigation for 2.0 replace plugin with apps framework

--- nautobot_ssot_dna_center/navigation.py
+++ nautobot_ssot_dna_center/navigation.py
@@ -1,27 +1,35 @@
 """Menu items."""
 
-from nautobot.extras.plugins import PluginMenuButton, PluginMenuItem
-from nautobot.utilities.choices import ButtonColorChoices
+from nautobot.apps.ui import NavMenuGroup, NavMenuItem, NavMenuTab, NavMenuAddButton, NavMenuImportButton
+
 
 menu_items = (
-    PluginMenuItem(
-        link="plugins:nautobot_ssot_dna_center:dnacinstance_list",
-        link_text="Nautobot SSoT for Cisco DNA Center",
-        buttons=(
-            PluginMenuButton(
-                link="plugins:nautobot_ssot_dna_center:dnacinstance_add",
-                title="Add",
-                icon_class="mdi mdi-plus-thick",
-                color=ButtonColorChoices.GREEN,
-                permissions=["nautobot_ssot_dna_center.add_dnacinstance"],
+    NavMenuTab(
+        name="DNA_SSoT",
+        groups=[
+            NavMenuGroup(
+                name="Device",
+                items=[
+                    NavMenuItem(
+                        link="plugins:nautobot_ssot_dna_center:dnacinstance_list",
+                        name="Nautobot SSoT for Cisco DNA Center",
+                        buttons=(
+                            NavMenuAddButton(
+                                link="plugins:nautobot_ssot_dna_center:dnacinstance_add",
+                                title="Add",
+                                icon_class="mdi mdi-plus-thick",
+                                permissions=["nautobot_ssot_dna_center.add_dnacinstance"],
+                            ),
+                            NavMenuImportButton(
+                                link="plugins:nautobot_ssot_dna_center:dnacinstance_import",
+                                title="Bulk Import",
+                                icon_class="mdi mdi-database-import-outline",
+                                permissions=["nautobot_ssot_dna_center.add_dnacinstance"],
+                            ),
+                        ),
+                    ),
+                ],
             ),
-            PluginMenuButton(
-                link="plugins:nautobot_ssot_dna_center:dnacinstance_import",
-                title="Bulk Import",
-                icon_class="mdi mdi-database-import-outline",
-                color=ButtonColorChoices.BLUE,
-                permissions=["nautobot_ssot_dna_center.add_dnacinstance"],
-            ),
-        ),
+        ],
     ),
 )
